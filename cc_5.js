eb12ed705fd6b2014e87c5bd8c8bc4cfbf6abc78
Cyrus Chinoy <cchinoy@usf.edu>
2025-06-22 20:51:28 -0400

    consistent commit 3

--- cc_5.js
+++ cc_5.js
@@ -1,7 +1,7 @@
 //step2
-const Employee1 = {name:"Bob Reed", hourlyRate: 10.00, hoursWorked: 30};
+const Employee1 = {name:"Bob Reed", hourlyRate: 10.00, hoursWorked: 20};
 const Employee2 = {name:"Dave Port", hourlyRate: 20.00, hoursWorked: 40};
-const Employee3 = {name:"Lee ", hourlyRate: 25.00, hoursWorked: 20};
+const Employee3 = {name:"Lee ", hourlyRate: 25.00, hoursWorked: 50};
 const Employee4 = {name:"Kim Johnson", hourlyRate: 15.00, hoursWorked: 25};
 let employees=[Employee1,Employee2,Employee3,Employee4];
 console.log("step 2 completed");
@@ -17,3 +17,18 @@ function calculateBasePay(rate, hours){
 console.log("step 3 completed");
 //step4
 function calculateOvertimePay(rate, hours){
+    var overtimeHours;
+    if(hours>40)
+    overtimeHours=hours-40; 
+    else
+    overtimeHours=0;
+    return
+    (overtimeHours*rate*1.5);
+}
+console.log("step 4 completed");
+//step 5
+calculateTaxes(grossPay) {
+    return(grosspay*0.15);
+}
+console.log("step 5 completed");
+//step 6
