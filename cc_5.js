1003b2fbb1aef1e1b1b36159baa0fd88090ddac5
Cyrus Chinoy <cchinoy@usf.edu>
2025-06-22 22:21:22 -0400

    final grade required rubric commit cc5

--- cc_5.js
+++ cc_5.js
@@ -17,29 +17,37 @@ function calculateBasePay(rate, hours){
 console.log("step 3 completed");
 //step4
 function calculateOvertimePay(rate, hours){
-    var overtimeHours;
+    var overtimeHours = 0;
     if(hours>40)
     overtimeHours=hours-40; 
-    else
-    overtimeHours=0;
-    return
-    (overtimeHours*rate*1.5);
+    return(overtimeHours*rate*1.5);
 }
 console.log("step 4 completed");
 //step 5
 function calculateTaxes(grossPay) {
-    return(grosspay*0.15);
+    return(grossPay*0.15);
 }
 console.log("step 5 completed");
 //step 6
 function processPayroll(employee) {
-    var employeePay = {name:"", basePay: 0, overtimePay:0, grossPay: 0, netPay:0};
-    employeePay.name = employee.name;
-    employeePay.basePay=calculateBasePay(employee.rate,employee.hours);
-    employeePay.overtimePay=calculateOvertimePay(employee.rate,employee.hours);
+    var employeePay = {name: "", basePay: 0, overtimePay: 0, grossPay: 0, netPay:0};
+    employeePay.name=employee.name
+    employeePay.basePay=calculateBasePay(employee.hourlyRate,employee.hoursWorked);
+    employeePay.overtimePay=calculateOvertimePay(employee.hourlyRate, employee.hoursWorked);
     var grossPay=employeePay.basePay+employeePay.overtimePay;
     employeePay.grossPay=grossPay;
     employeePay.netPay=grossPay-calculateTaxes(grossPay);
     return employeePay;
 }
 console.log("step 6 completed");
+//step 7
+console.log("");
+for(let employee of employees) {
+    for(let [index,item] of Object.entries(employee)) {
+    const payroll=processPayroll(employee);
+        for(let [count,nItem] of Object.entries(payroll)) {
+        console.log(`${count}:${nItem}`);
+        }
+    }
+}
+console.log("step 7 completed");
