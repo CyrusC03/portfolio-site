65e1d110329f2ac9b35089bda69152ae1fbf9e94
Cyrus Chinoy <cchinoy@usf.edu>
2025-06-29 19:58:04 -0400

    commit 1

--- cc_6.js
+++ cc_6.js
@@ -1,4 +1,4 @@
-//step2
+//step 2
 class Employee {
   constructor(name, department) {
      // Initialize properties here
@@ -11,42 +11,5 @@ class Employee {
              console.log(`Hello, my name is ${this.name} and I am from ${this.department} department.`);
    }
 }
+
 //step 3
-class Manager extends Employee {
-    constructor(name, department, teamSize) {
-        // Initialize properties here
-
-        super(name,department);
-
-        this.teamSize=teamSize;
-    }
-    // Define methods here
-    describe() {
-        // Method code
-
-         console.log(`Hello, my name is ${this.name} and I am from ${this.department} department. My team's size is ${this.teamSize}`);
-    }
-}
-//step 4
-
-class Manager extends Employee {
-    constructor(name, department, teamSize) {
-        // Initialize properties here
-
-        super(name,department);
-
-        this.teamSize=teamSize;
-    }
-    // Define methods here
-    describe() {
-        // Method code
-
-         console.log(`Hello, my name is ${this.name} and I am from ${this.department} department. My team's size is ${this.teamSize}`);
-    }
-}
-//step 4
-
-const employee1 = new Employee("John Doe", "Information Technology");
-const employee2 = new Employee("Jane Sioth", "Accounts payable");
-const employee3 = new Employee("Bob Dylan", "Operations");
-const manager 1 = new Manager("Cyrus Chinoy", "Operations", 3);
